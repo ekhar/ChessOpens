96732b2b6688efa9c701df197fd6fa63a32531b2
Ubuntu <ubuntu@ip-172-31-21-150.ec2.internal>
2021-01-19 09:27:37 +0000

    inside amazon

--- ChessOpens/__init__.py
+++ ChessOpens/__init__.py
@@ -4,10 +4,15 @@ from authlib.integrations.flask_client import OAuth
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
-app.secret_key = "hello"
+app.config["SQLALCHEMY_DATABASE_URI"] = config.get("SQLALCHEMY_DATABASE_URI")
+app.secret_key = config.get("SECRET_KEY")
 db = SQLAlchemy(app)
 
 oauth = OAuth(app)
